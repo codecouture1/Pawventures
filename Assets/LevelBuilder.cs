3fa6d5cd9a298cabb0212049b9fcc76ca41ecfdf
llukass3 <167030343+llukass3@users.noreply.github.com>
2024-12-28 16:50:23 +0100

    Commit Fix

--- Assets/LevelBuilder.cs
+++ Assets/LevelBuilder.cs
@@ -1,16 +1,46 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class LevelBuilder : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //TODO: Coins und PowerUp platzieren und diese mit einer bestimmten wahrscheinlichkeit spawnen lassen
+
+    public GameObject[] prefab;
+    private GameObject current;
+    private GameObject previous;
+    private float spawnAtX = 0;
+
     void Start()
     {
-        
+        for (int c = 0; c < 10; c++)
+        {
+            current = prefab[Random.Range(0, prefab.Length)];
+            if(previous != null)
+            {
+                spawnAtX += GetPrefabWidth(previous);
+                Instantiate(current, new Vector3(spawnAtX, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(current, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+            previous = current;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    float GetPrefabWidth(GameObject prefab)
     {
-        
+        // Get the Renderer component of the prefab
+        Renderer renderer = prefab.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Return the width of the prefab (size in the x-axis)
+            return renderer.bounds.size.x;
+        }
+
+        // If no Renderer is found, return a default width
+        return 1f;
     }
 }
