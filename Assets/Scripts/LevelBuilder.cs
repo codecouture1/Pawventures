7dc767ad88c71f54c84ddcdf547ed40a27e892ef
llukass3 <167030343+llukass3@users.noreply.github.com>
2024-12-29 21:59:36 +0100

    LevelBuilder 1.0 fertig

---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,93 @@
+using NUnit.Framework.Constraints;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// TODO: modul-auswahl verbessern
+
+public class LevelBuilder : MonoBehaviour
+{
+    private Transform parentTransform; //transform of the empty parentobject
+
+    public int numberOfModules; //the number of modules to be spawned
+    public float coinSpawnChance;
+    public float powerupSpawnChance;
+
+    //Array containing all prefabs that can be spawned. Will be assigned to Modules in modules[]
+    public GameObject[] modulePrefabs;
+    private LevelModule[] modules;
+    public GameObject finalModule; //the last module to be spawned
+
+    private LevelModule currentModule;
+    private GameObject currentPrefab; //the current prefab spawn
+    private LevelModule previous; //the previous prefab spawn
+    private float x = 0; //the x coordinate of the current spawn
+
+    private void Awake()
+    {
+        parentTransform = GetComponent<Transform>();
+        modules = AssignPrefabs();
+    }
+
+    void Start()
+    {
+        BuildLevel();
+    }
+
+    //spawn Level Modules in a row and in random order
+    private void BuildLevel()
+    {
+        for (int c = 0; c < numberOfModules; c++)
+        {
+            Debug.Log($"---------------Current Module : {c}.---------------");
+            //pick a random Module from modules[]
+            currentModule = modules[Random.Range(0, modules.Length)];
+
+            //spawn prefab and attatch it to the parent object
+            currentModule.instance = Instantiate(currentModule.prefab, new Vector3(x, 0, 0), Quaternion.identity, parentTransform);
+
+            //calculate spawn position for next module (-0.05f for seamless blending)
+            x += currentModule.GetWidth() - 0.05f;
+  
+
+            /*TODO: spawn Polaroid
+            currentModule.Spawn(LevelModule.Collectible.Polaroid)
+            */
+
+            currentModule.Spawn(LevelModule.Collectible.PowerUp, ChanceOf(powerupSpawnChance));
+            Debug.Log(currentModule.ContainsPowerUp);
+            if(!currentModule.ContainsPowerUp)
+                currentModule.Spawn(LevelModule.Collectible.Coins, ChanceOf(coinSpawnChance));
+
+            //update previous spawn for next loop iteration
+            previous = currentModule;
+        }
+
+        //spawn final module
+        Instantiate(finalModule, new Vector3(x, 0, 0), Quaternion.identity, parentTransform);
+    }
+
+    private LevelModule[] AssignPrefabs()
+    {
+        LevelModule[] modules = new LevelModule[modulePrefabs.Length];
+        int index = 0;
+
+        foreach (GameObject modulePrefab in modulePrefabs)
+        {
+            modules[index] = new LevelModule(modulePrefab);
+
+            index++;
+        }
+
+        return modules;
+    }
+
+    //return value is determined by the input float (if chance = 0.25f it will return true 25% of the time)
+    private bool ChanceOf(float chance)
+    {
+        if (Random.Range(0f, 1f) <= chance)
+            return true;
+
+        else return false;
+    }
+}
---
+++ Assets/Scripts/LevelModule.cs
@@ -0,0 +1,100 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class LevelModule
+{
+    public enum Collectible
+    {
+        None,
+        Coins,
+        PowerUp,
+        Polaroid,
+    }
+
+    public readonly float width;
+
+    public GameObject prefab; //the prefab that this module represents
+    public GameObject instance; //the instance of this modules prefab
+
+    GameObject coins;
+    GameObject powerUp;
+
+    public bool ContainsPolaroid { get; private set; }
+    public bool ContainsCoins { get; private set; } = false;
+    public bool ContainsPowerUp { get; private set; } = false;
+
+    private const int coinIndex = 0; //the coin container must always have the index 0 within the prefab
+    private const int powerUpIndex = 1; //the powerup container must always have the index 0 within the prefab
+
+    public LevelModule(GameObject prefab)
+    {
+        this.prefab = prefab;
+        width = GetWidth();
+    }
+
+
+    public float GetWidth()
+    {
+        // Get the Renderer component of the prefab
+        Renderer renderer = prefab.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Return the width of the prefab (size in the x-axis)
+            return renderer.bounds.size.x;
+        }
+
+        // If no Renderer is found, return a default width
+        return 1f;
+    }
+
+    public void Spawn(Collectible collectible, bool condition)
+    {
+        switch (collectible)
+        {
+            case Collectible.Coins:
+                SpawnCoins(condition);
+                break;
+            case Collectible.PowerUp:
+                SpawnPowerUp(condition);
+                break;
+            case Collectible.Polaroid:
+                SpawnPolaroid();
+                break;
+        }
+    }
+
+    private void SpawnCoins(bool condition)
+    {
+        coins = instance.transform.GetChild(coinIndex).gameObject;
+        if (coins.CompareTag("CoinContainer")) //checks if object is a CoinContainer 
+        {
+            ContainsCoins = false;
+            if (condition)
+            {
+                coins.SetActive(true);
+                ContainsCoins = true;
+            }
+        }              
+    }
+
+    private void SpawnPowerUp(bool condition)
+    {
+        powerUp = instance.transform.GetChild(powerUpIndex).gameObject;
+        if (powerUp.CompareTag("PowerUp")) //checks if object is a PowerUp
+        {
+            ContainsPowerUp = false;
+            if (condition)
+            {
+                powerUp.SetActive(true);
+                ContainsPowerUp = true;
+            }
+        }                 
+    }
+
+    // TODO: implement
+    private void SpawnPolaroid()
+    {
+        ContainsPolaroid = true;
+    }
+
+}
