30c6ed3bc23473997f7f753f318e7646be87232f
llukass3 <167030343+llukass3@users.noreply.github.com>
2024-12-29 02:40:39 +0100

    Level Generator

--- Assets/LevelBuilder.cs
+++ Assets/LevelBuilder.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -5,30 +6,60 @@ using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
 {
-    //TODO: Coins und PowerUp platzieren und diese mit einer bestimmten wahrscheinlichkeit spawnen lassen
+    private Transform parentTransform; //transform of the empty parentobject
 
-    public GameObject[] prefab;
-    private GameObject current;
-    private GameObject previous;
-    private float spawnAtX = 0;
+    public int numberOfModules; //the number of modules to be spawned
+    public float coinSpawnProbability;
+    public float powerupSpawnProbability;
+    public GameObject[] modules; //Array containing all modules that can be spawned
+
+    private GameObject currentPrefab; //the current prefab spawn
+    private GameObject previous; //the previous prefab spawn
+    private float x = 0; //the x coordinate of the current spawn
+
+    private const int coinIndex = 0; //the coin container must always have the index 0 within the prefab
+    private const int powerUpIndex = 1; //the powerup container must always have the index 0 within the prefab
+
+    private void Awake()
+    {
+        parentTransform = GetComponent<Transform>();
+    }
 
     void Start()
     {
-        for (int c = 0; c < 10; c++)
+        for (int c = 0; c < numberOfModules; c++)
         {
-            current = prefab[Random.Range(0, prefab.Length)];
-            if(previous != null)
+            //pick a random prefab from modules[]
+            currentPrefab = modules[Random.Range(0, modules.Length)];
+
+            if(previous != null)            
+                x += GetPrefabWidth(previous);               
+
+            //spawn prefab and attatch it to the parent object
+            GameObject currentCopy = Instantiate(currentPrefab, new Vector3(x, 0, 0), Quaternion.identity, parentTransform);
+
+            // Spawn coins or powerup
+            if (SpawnPowerUp(currentCopy, ChanceOf(powerupSpawnProbability)))
             {
-                spawnAtX += GetPrefabWidth(previous);
-                Instantiate(current, new Vector3(spawnAtX, 0, 0), Quaternion.identity);
+                // PowerUp spawned, ensure no coins are spawned
+                Debug.Log($"PowerUp Spawned at {c}. No coins here.");
+            }
+            else if (SpawnCoins(currentCopy, ChanceOf(coinSpawnProbability)))
+            {
+                // Coins spawned, ensure no powerup exists
+                Debug.Log($"Coins Spawned at {c}. No powerups here.");
             }
             else
             {
-                Instantiate(current, new Vector3(0, 0, 0), Quaternion.identity);
+                Debug.Log($"Neither Coins nor PowerUp spawned at {c}.");
             }
-            previous = current;
+
+            //update previous spawn for next loop iteration
+            previous = currentPrefab;
         }
     }
+
+
 
     float GetPrefabWidth(GameObject prefab)
     {
@@ -42,5 +73,37 @@ public class LevelBuilder : MonoBehaviour
 
         // If no Renderer is found, return a default width
         return 1f;
+    }
+
+    private bool SpawnCoins(GameObject currentCopy, bool condition)
+    {
+        if (condition)
+        {
+            GameObject coins = currentCopy.transform.GetChild(coinIndex).gameObject;
+            if(coins.CompareTag("CoinContainer")) //checks if object is a CoinContainer        
+               coins.SetActive(true);
+            return true; //coins were spawned
+        }
+        return false; //coins were NOT spawned
+    }
+
+    private bool SpawnPowerUp(GameObject currentCopy, bool condition)
+    {
+        if (condition)
+        {
+            GameObject powerUp = currentCopy.transform.GetChild(powerUpIndex).gameObject;
+            if (powerUp.CompareTag("PowerUp")) //checks if object is a PowerUp
+                powerUp.SetActive(true);
+            return true; //powerup was spawned
+        }
+        return false; //powerup was NOT spawned
+    }
+
+    private bool ChanceOf(float chance)
+    {
+        if (Random.Range(0f, 1f) <= chance)
+            return true;
+
+        else return false;
     }
 }
